74bc02079ecba50ac46f7d6c487de9717885692f
Eddy98 <eduardodgraziano@gmail.com>
2021-10-24 20:06:25 -0400

    weather api

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import { SocialIcon } from 'react-social-icons';
-import { Wrapper } from './styles';
-import { Col } from 'react-bootstrap';
+import { weather } from '../../api/endpoints.json';
+import { Wrapper, WeatherWrapper, WeatherText } from './styles';
+import Col from 'react-bootstrap/Col';
+import Media from 'react-media';
 
 const Contact = () => {
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      let res = await fetch(weather.url);
+      res = await res.json();
+      setState({
+        city: res.name,
+        temp: res.main.temp,
+        description: res.weather[0].description
+          .toLowerCase()
+          .split(' ')
+          .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(' '),
+      });
+    }
+    fetchData();
+  }, []);
+
   return (
     <Wrapper>
-      <Col sm={12}>
+      <Col sm={8}>
         <SocialIcon
           url='https://www.linkedin.com/in/eduardo-graziano-7795ab166/'
           target='_blank'
@@ -24,6 +46,31 @@ const Contact = () => {
           bgColor='#cf2525'
           style={{ height: 60, width: 60 }}
         />
+      </Col>
+      <Col sm={4}>
+        <Media queries={{ small: { maxWidth: 575 } }}>
+          {(matches) =>
+            matches.small ? (
+              <></>
+            ) : (
+              <>
+                {state && (
+                  <WeatherWrapper>
+                    <WeatherText>
+                      {`What's the weather like in ${state.city}?`}
+                      <br />
+                      <b>{`${state.description}, ${Math.round(
+                        state.temp
+                      )} `}</b>
+                      <span>&#8457;</span>
+                    </WeatherText>
+                    <br />
+                  </WeatherWrapper>
+                )}
+              </>
+            )
+          }
+        </Media>
       </Col>
     </Wrapper>
   );
--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -7,4 +7,16 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 
-export { Wrapper };
+const WeatherWrapper = styled.div`
+  display: flex;
+  justify-content: right;
+`;
+
+const WeatherText = styled.p`
+  font-size: 1rem;
+  font-family: 'Nunito';
+  margin: 0 0 1rem;
+  text-align: right;
+`;
+
+export { Wrapper, WeatherWrapper, WeatherText };
